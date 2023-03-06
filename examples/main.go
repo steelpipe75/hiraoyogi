32b688b241b9a06f25e081f5db602f8cf7f3100d
Nishizaki <steelpipe75@gmail.com>
2023-03-06 22:17:48 +0900

    elementの追加まで対応

--- examples/main.go
+++ examples/main.go
@@ -6,12 +6,52 @@ import (
 	"github.com/steelpipe75/hiraoyogi"
 )
 
-func main() {
-	filename := "example.puml"
+type TestElement struct {
+	Name string
+	Type string
+}
+
+func listExample() {
+	filename := "listExample.puml"
 	writer, e := hiraoyogi.New(filename)
 	if e != nil {
 		panic(e)
 	}
 	defer writer.Close()
+	elementSlice := []hiraoyogi.ElementType{}
+
+	testSlice := []TestElement{}
+	testSlice = append(testSlice, TestElement{Name: "test analog", Type: "analog"})
+	testSlice = append(testSlice, TestElement{Name: "test binary", Type: "binary"})
+	testSlice = append(testSlice, TestElement{Name: "test concise", Type: "concise"})
+	testSlice = append(testSlice, TestElement{Name: "test robust", Type: "robust"})
+
+	for _, t := range testSlice {
+		element := writer.NewElement(t.Name, t.Type)
+		elementSlice = append(elementSlice, element)
+	}
+
+	writer.RegisterElementList(elementSlice)
+}
+
+func vParamExample() {
+	filename := "vParamExample.puml"
+	writer, e := hiraoyogi.New(filename)
+	if e != nil {
+		panic(e)
+	}
+	defer writer.Close()
+
+	writer.RegisterElements(
+		writer.NewElement("test analog", "analog"),
+		writer.NewElement("test binary", "binary"),
+		writer.NewElement("test concise", "concise"),
+		writer.NewElement("test robust", "robust"),
+	)
+}
+
+func main() {
+	listExample()
+	vParamExample()
 	fmt.Println("Hello World!")
 }
--- writer.go
+++ writer.go
@@ -2,8 +2,12 @@ package hiraoyogi
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strings"
 )
+
+var supportedTypes = []string{"analog", "binary", "concise", "robust"}
 
 type HiraoyogiError struct {
 	msg string
@@ -16,6 +20,12 @@ func (error *HiraoyogiError) Error() string {
 type HiraoyogiWriter struct {
 	loadedFile *os.File
 	buffered   *bufio.Writer
+}
+
+type ElementType struct {
+	Name       string
+	Type       string
+	identifier string
 }
 
 func New(filename string) (HiraoyogiWriter, error) {
@@ -31,6 +41,15 @@ func New(filename string) (HiraoyogiWriter, error) {
 	return writer, err
 }
 
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -42,6 +61,26 @@ func check2(data interface{}, e error) interface{} {
 		panic(e)
 	}
 	return data
+}
+
+func (hWriter *HiraoyogiWriter) NewElement(elementName string, typeName string) ElementType {
+	if !stringInSlice(typeName, supportedTypes) {
+		errorStr := fmt.Sprintf("unsupported data type: \"%s\" supported types: %v", typeName, supportedTypes)
+		panic(errorStr)
+	}
+	return ElementType{Name: elementName, Type: typeName, identifier: strings.Replace(elementName, " ", "_", -1)}
+}
+
+func (hWriter *HiraoyogiWriter) RegisterElements(elements ...ElementType) error {
+	for _, element := range elements {
+		check2(hWriter.buffered.WriteString(element.Type + " \"" + element.Name + "\" as " + element.identifier + "\n"))
+	}
+	check2(hWriter.buffered.WriteString("\n"))
+	return nil
+}
+
+func (hWriter *HiraoyogiWriter) RegisterElementList(elements []ElementType) error {
+	return hWriter.RegisterElements(elements...)
 }
 
 func (hWriter HiraoyogiWriter) Close() {
